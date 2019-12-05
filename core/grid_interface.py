9ba354d954b706799922e34f69722f49373384fe
f0cus10 <iakhan4425@gmail.com>
2019-12-05 00:01:53 -0500

    Generate shadow_map simultaneously for max performance

--- core/grid_interface.py
+++ core/grid_interface.py
@@ -1,11 +1,11 @@
 #Everything Class
 
-from Node import Node
-from baseStation import baseStation
 import math
 from collections import defaultdict 
 import sys 
 import random
+from Node import Node
+from baseStation import baseStation
 
 # #User input number of channels, ranging from 5 - 10
 # numOfChannels = 6
@@ -17,6 +17,7 @@ def generate_grid(param_dict):
 	#generate a zero 2D array
 	rows, cols = (100, 100)
 	GRID = [[0 for i in range(cols)] for j in range(rows)]
+	shadow_map = [['Z' for i in range(cols)] for j in range(rows)]
 
 	#Randomly plot nodes onto GRID
 	for node in range(param_dict['num_nodes']):
@@ -26,6 +27,7 @@ def generate_grid(param_dict):
 			x = random.randrange(0,99)
 			y = random.randrange(0,99)
 		GRID[x][y] = Node(x, y, node, param_dict['NODE_RADIUS'])
+		shadow_map[x][y] = 'N' + str(node)
 	
 	#Randomly plot base stations onto GRID
 	for base in range(param_dict['num_stations']):
@@ -35,6 +37,7 @@ def generate_grid(param_dict):
 			x = random.randrange(0,99)
 			y = random.randrange(0,99)
 		GRID[x][y] = baseStation(x, y, base, param_dict['BASE_STATION_RADIUS'])
+		shadow_map[x][y] = 'B' + str(node)
 		
 
-	return GRID
+	return GRID, shadow_map
