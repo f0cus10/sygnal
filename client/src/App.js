0c9c678aff7301deeb2e5e8c87f3218c6aabde02
Jasmine Wong <jasminewong@Jasmines-MacBook-Pro.local>
2019-12-05 00:02:00 -0500

    added initial network graph but no coordinate positioning

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from "react";
 import logo from './logo.svg';
 import "./App.css";
 import Form from "./Form";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Graph } from "react-d3-graph";
 
 class App extends Component {
 state = {
-  fields: {}
+  fields: {},
+  data: {
+    nodes: [ 
+    { id: "BS1", symbolType: "triangle"}, { id: "BS2", symbolType: "triangle"}, 
+    { id: "Node1"}, { id: "Node2"}, { id: "Node3"}, { id: "Node4"}],
+    links: [
+    { source: "BS1", target: "Node1" }, { source: "BS1", target: "Node3" },
+    { source: "BS2", target: "Node2" }, { source: "BS2", target: "Node4" }]
+  }
+
 };
 
   onSubmit = updatedValue => {
@@ -25,6 +34,11 @@ render () {
         <p>
         {JSON.stringify(this.state.fields, null, 2)}
         </p>
+        graph will go here
+        <Graph
+          id="graph-id"
+          data={this.state.data}
+        />
     </div>
   );
   }
