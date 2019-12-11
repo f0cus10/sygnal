6e9d7163e2892eacdee48bb390b9144c65d79b26
Jasmine Wong <jaznwong@gmail.com>
2019-12-10 21:17:21 -0500

    figured out how to dynamically add connections

--- client/src/App.js
+++ client/src/App.js
@@ -3,32 +3,25 @@ import logo from './logo.svg';
 import "./App.css";
 import Form from "./Form";
 import { XYPlot, CustomSVGSeries, LineSeries} from 'react-vis';
+import { grid } from "./files/grid.json";
+import { connections } from "./files/connections.json";
+import lineSeries from "react-vis/dist/plot/series/line-series";
+
+/*** what i need to do:
+-axios for GET request of nodes and BS
+-axios for GET request for connections
+-need an API call to post to a JSON file for source & destination links
+-figure out how to use the SVG component
+-need to parse through the data from GET request to produce the JSON
+ ***/
 
 class App extends Component {
 state = {
-  fields: {},
   gridData : [],
+  fields: {},
   gridId: null,
-  data: [
-  {x: 10, y: 20, customComponent: 'circle', size: 20, style: {fill:'orange'}},
-  {x: 40, y: 20 , customComponent: 'circle', size: 20, style: {fill:'orange'}},
-  {x: 70, y: 50, customComponent: 'square', size: 20},
-  {x: 55, y: 15, customComponent: 'square', size: 20}
-  ],
-  connectionData: [
-    {x: 70, y: 50},
-    {x: 55, y: 15}
-  ]
-
 };
 
-  // formSubmit = (serverData) => {
-  //   const {gridID, grid} = serverData;
-  //   this.setState({
-  //     gridId: gridID,
-  //     gridData: grid 
-  //   })
-  // }
 
 render () {
   return (
@@ -38,8 +31,12 @@ render () {
         {JSON.stringify(this.state.fields, null, 2)}
         </p>
         <XYPlot width={300} height={300}>
-          <CustomSVGSeries data = {this.state.data}/>
-          <LineSeries data = {this.state.connectionData}/>
+          <CustomSVGSeries data = {grid}/>
+          {Object.values(connections).map((index) => {
+            return(
+              <LineSeries data={index}/>
+            )
+          })}
         </XYPlot>
     </div>
   );
