6b24cdabd6d88832499b83519f3283104fba14a2
f0cus10 <iakhan4425@gmail.com>
2019-12-18 11:44:23 -0500

    Refactor Graph display

--- client/src/Graph.js
+++ client/src/Graph.js
@@ -1,22 +1,32 @@
 import React, { Component } from "react";
-import logo from './logo.svg';
 import "./App.css";
-import Form from "./Form";
 import { XYPlot, CustomSVGSeries, LineSeries} from 'react-vis';
 import { grid } from "./files/grid.json";
 import { connections } from "./files/connections.json";
-import lineSeries from "react-vis/dist/plot/series/line-series";
 
-render () {
-<p>
-{JSON.stringify(this.state.fields, null, 2)}
-</p>
-<XYPlot width={300} height={300}>
-  <CustomSVGSeries data = {grid}/>
-  {Object.values(connections).map((index) => {
+class Graph extends Component {
+  state = {
+    fields: {}
+  }
+  render(){
     return(
-      <LineSeries data={index}/>
+      <div>
+        <p>
+          {JSON.stringify(this.state.fields, null, 2)}
+        </p>
+        <XYPlot width={300} height={300}>
+          <CustomSVGSeries data={grid} />
+          {
+            Object.values(connections).map((i) => {
+              return(
+                <LineSeries data={i} />
+              );
+            })
+          }
+        </XYPlot>
+      </div>
     )
-  })}
-</XYPlot>
+  }
 }
+
+export default Graph;
