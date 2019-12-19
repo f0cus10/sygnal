8327895077f0881cd75d416447cff703a7e927cb
sanjaysubhag <sanjaysubhag@gmail.com>
2019-12-19 03:05:50 -0500

    graphical form input still needs redirects

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,35 +1,33 @@
 import React from "react";
 import axios from "axios";
 import "./App.css";
+import { Redirect } from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {withRouter} from 'react-router-dom';
 
 export default class Form extends React.Component {
   state = {
     numChannels: "",
    };
 
+   change = e => {
+      this.setState({
+          [e.target.name]: e.target.value
+              });
+                };
 
-change = e => {
-  this.setState({
-    [e.target.name]: e.target.value
-  });
-};
+    onSubmit = async(e) => {
+        e.preventDefault();
+        const { numChannels } = this.state;
 
-onSubmit = async(e) => {
-  e.preventDefault();
-  const { numChannels } = this.state;
+        const result = await axios.post('/submitform', {numChannels});
+        const response = result.data;
+        console.log(response);
+        //this.props.updateGrid(response)
 
-  const result = await axios.post('/submitform', {numChannels});
-  const response = result.data;
-  console.log(response);
-  //this.props.updateGrid(response)
-  this.setState({
-      numChannels: "",
-      });
-
-};
+      };
 
 render () {
-
   return (
   <div className = "wrapper">
     <h1> CSCI 39596 Project: Iftikhar, Jasmine, Kenny, Sami, Sanjay, Xiangmin </h1>
@@ -42,7 +40,7 @@ render () {
     onChange={e => this.change(e)}
     />
     <br />
-    <button onSubmit={e => this.onSubmit(e)}>Submit</button>
+    <button onSubmit= {e => this.onSubmit} >Submit</button>
   </form>
 </div>
     );
