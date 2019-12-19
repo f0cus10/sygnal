7b9958d4d4aa24999cd81df6557df39d5e6efbe4
f0cus10 <iakhan4425@gmail.com>
2019-12-19 10:34:26 -0500

    Add in DFS algo

--- server/graph.py
+++ server/graph.py
@@ -4,3 +4,28 @@ class Graph:
   def __init__(self, vertices):
     self.V = vertices
     self.graph = [None] * self.V
+
+#BFS algorithm
+def BFS(start, goal, node_dict):
+        explored = []
+        queue = [[start]]
+
+        if start == goal:
+          return "Start = goal"
+
+        while queue:
+
+          path = queue.pop(0)
+          node = path[-1]
+          if node not in explored:
+            neighbours = node_dict[node]
+            for neighbor in neighbours:
+              new_path = list(path)
+              new_path.append(neighbor)
+              queue.append(new_path)
+              if neighbor == goal:
+                return new_path
+            explored.append(node)
+        return "No path!"
+
+
